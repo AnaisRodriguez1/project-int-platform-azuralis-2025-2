a1ea5ae1c26bdbaf61842365833554b88dc56b4e
AnaisRodriguez1 <anaisrodriguezcarvajal@gmail.com>
2025-10-26 22:19:38 -0300

    borrar todo

--- backend/src/patients/dto/emergency-access.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class EmergencyAccessDto {
-  @IsString()
-  @IsNotEmpty()
-  rut: string;
-}
--- backend/src/shared/enums/care-team-role.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum CareTeamRole {
-  ONCOLOGO_PRINCIPAL = 'oncologo_principal',
-  CIRUJANO = 'cirujano',
-  RADIOLOGO = 'radiologo',
-  ENFERMERA_JEFE = 'enfermera_jefe',
-  CONSULTOR = 'consultor',
-}
